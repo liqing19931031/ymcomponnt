df0cd0c30c9c8d372c5da2cacecf2ef84e0623f3
liqing931031 <liqing@qq.com>
2023-03-02 09:13:21 +0800

    feat: update submit

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path')
 module.exports = {
     entry: './index.js',
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: "main.js",
-        libraryTarget: "umd",
-        library: 'ym-comonent',
+        library: {
+          name: "YmCop",
+          type: "umd"
+        },
     },
     externals: {
         'react': 'react',
